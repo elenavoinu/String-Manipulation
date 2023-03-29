d7cc027770685c61423efb4e029fced3fcf319db
Elena Voinu <37947819+elenavoinu@users.noreply.github.com>
2023-03-29 13:13:17 -0500

    Add files via upload

---
+++ IndexOfTheFirstOccurrence.java
@@ -0,0 +1,22 @@
+public class IndexOfTheFirstOccurrence {
+    public static void main(String[] args) {
+        String input = "sadbutsad";
+        String input2 = "sad";
+        System.out.println(strStr("haystack", "needle"));
+        System.out.println(strStr(input, input2));
+    }
+    public static int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        int haystackLen = haystack.length();
+        int needleLen = needle.length();
+        for (int i = 0; i <= haystackLen - needleLen; i++) {
+            if (haystack.substring(i, i + needleLen).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
